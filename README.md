f1fabb113d06b9bb26ca3d8023ac782c0f674142
NayLuna <nayelliluna21@gmail.com>
2024-08-11 15:14:38 -0600

    Presentación y habilidades
    Se terminó de estructurar la presentación y habilidades

---
+++ README.md
@@ -0,0 +1,23 @@
+# Portafolio de habilidades TECHNOLOCHICAS PRO
+
+El presente proyecto es un portafolio desarrollado para poner en práctica las habilidades obtenidad dentro del bootcamp de desarrollo frontend de Technolochicas PRO.
+
+Fue desarrollado con HTML, CSS y JS con el uso de el framework de UI, Bootstrap utilizando además bibliotecas externas.
+
+La página es responsiva (adaptable a diferentes tamaños de pantalla) e incluye la presentación de la autora del proyecto.
+
+[Proyecto Desplegado (poner link de proyecto aquí)](link otra vez aquí)
+
+## Secciones de mi sitio
+
+![Presentación](link de la imagen)
+
+## Tecnologías
+
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+
+---
+Desarrollado con 💕 por [Luna] (https://www.instagram.com/luna_bess_/) en [TECHNOLOCHICAS PRO](https://tecnolochicas.mx/).
---
+++ script.js
@@ -0,0 +1,23 @@
+// alert("Hola Mundo <3");
+
+let app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+    loop: true, // se ejecutará todo el tiempo
+    delay: 75,
+});
+
+// reconoces una acción porque tiene parentesis después del nombre de la acción
+// a veces tiene el operador punto (.)
+typewriter
+    .pauseFor(2500) // milisegundos 2.5 segundos
+    .typeString('Nayelli Luna')
+    .pauseFor(300) // que haga una pausa
+    // .deleteChars(10) // borrar letras
+    .deleteAll () // debe llevar parentesis para que sea un método, ya que sin ellos es una propiedad
+    .typeString('Desarrolladora FRONTED JR')
+    .pauseFor(500)
+    .deleteAll (9)
+    .typeString('Desarrolladora de negocios, área Mercadotecnia')
+    .pauseFor(1000) // pausa para volver a escribir
+    .start(); // inicia el efecto
